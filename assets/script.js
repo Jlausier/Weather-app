402d39de6bc88028253f603641f06de665ea3e4a
Jacob <jakelausier7878@gmail.com>
2023-07-11 16:38:44 -0400

    display fucntion and geocode location.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var weather = document.getElementById('searched-day')
+var weather = document.getElementById('searched-city')
 var fiveDay = document.getElementById('5-day')
 var listEl = document.getElementById('previous')
 var farenheit = document.getElementById('Farenheit')
@@ -24,20 +24,34 @@ function getWeather(){
         + "&units=metric&appid=" 
         + this.myApi
     )
-    
-    .then((response) => response.json())
-    .then((data) => console.log(data)) 
+    .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayCurrent(data));
     
     
 displayCurrent: function(data){
     const {name} = data;
-    const {icon, description} = data.weather;
+    const {icon, description} = data.weather[0];
+    const {temp, humidity}= data.main;
+    weather.innerText = "Weather in " + name;
+    document.getElementById('icon').src = "https://openweathermap.org/img/wn/" + icon + ".png"
+    document.getElementById('temp').innerText = temp +" °C"
+    document.getElementById('description').innerText = description
+    document.getElementById('humidity').innerText = humidity
+
 
 }
     
 }
 
-
+function search(){
+    this.getWeather(document.getElementById('input').value)
+}
 // function to display 5 day forcast
 
 
@@ -51,6 +65,57 @@ displayCurrent: function(data){
 
 
 //function for geolocation and use function to make calls above
+let geocode ={
+    reverseGeocode: function(latitude, longitude){
+    var apiUrl = 'https://api.opencagedata.com/geocode/v1/json';
 
+    var requestUrl = apiUrl
+    + "?"
+    + "key="
+    + myApi
+    + "&q="
+    + encodeURIComponent(latitude + "," + longitude)
+    + "&pretty=1"
+    + "&no_annotations=1"
+
+    var request = new XMLHttpRequest();
+    request.open("GET", requestUrl, true);
+
+    request.onload = function () {
+
+      if (request.status == 200) {
+        var data = JSON.parse(request.responseText);
+        weather.getWeather(data.results[0].components.city);
+        console.log(data.results[0].components.city)
+      } else if (request.status <= 500) {
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      console.log("unable to connect to server");
+    };
+
+    request.send(); 
+  },
+  getLocation: function() {
+    function success (data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    }
+    else {
+      weather.getWeather("orlando");
+    }
+  }
+};
+
+geocode.getLocation();
 // function to create buttons on search history
 
