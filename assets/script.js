b78a37088fefee0700f2cc88449d4dc97e1e0abe
Jacob <Jakelausier7878@gmail.com>
2023-07-12 13:20:53 -0400

    working on 5-day forcast

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,10 @@ $(buttonEl).on('click',getWeather)
 
 
 
-function getWeather(){
-    
-    var city = (document.getElementById('city').value)
+function getWeather(event){
+    event.preventDefault();
+
+    var city = (document.getElementById('city').value.trim())
     fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" 
         + city 
@@ -62,6 +63,7 @@ function forcast(){
     "https://api.openweathermap.org/data/2.5/forecast?" +
      "lat=" + lat+
      "&lon=" + lon+
+     "&units=imperial&appid=" +
      "&appid=" + myApi
  ).then((response) => {
     return response.json();
@@ -73,7 +75,26 @@ function displayFuture(){
     const {name} = data;
     const {icon, description} = data.weather[0];
     const {temp, humidity}= data.main;
+    const {date}= data.list.dt
+    data.forEach(element => {
+        
+    
+       historyDiv =   document.createElement('div')
+       historyDate = document.createElement('h2')
+       historyTemp =  document.createElement('h2')
+       historyImg =  document.createElement('img')
+       historyDes =  document.createElement('h2')
+       historyHum =  document.createElement('h2')
+
+       historyDiv.id = "future"
+       historyDate.id ="date"
+       historyTemp.id = "temp-2"
+       historyImg.id = "icon-2"
+       historyDes.id = "description-2"
+       historyHum.id = "humidity-2"
+    });
     fiveDay.innerText =  + name;
+    document.getElementById('date').innerText = date
     document.getElementById('icon-2').src = "https://openweathermap.org/img/wn/" + icon + ".png"
     document.getElementById('temp-2').innerText = temp +" °F"
     document.getElementById('description-2').innerText = description
@@ -83,15 +104,45 @@ function displayFuture(){
 }
 
 //array of search history
-localStorage.setItem('city', JSON.stringify(city))
-//function to display search history
+let storeHistory = [];
 
+//function to display search history
+// function to create buttons on search history
+function displayHistory(){
+    for( var i = 0; i <localStorage.length; i++){
+        var history = localStorage.key(i);
+        storeHistory.push({text: history})
+        historyList = document.createElement('div')
+        history.classList.add('btn, col-4, shadow-0 , p-2, text-dark-emphasis, fs-4, bg-info-subtle')
+        historyList.innerText = history
+        listEl.append(historyList)
+        $(listEl).on('click', weatherAgain)
+    }
+    function weatherAgain(event){
+        event.preventDefault();
+
+        var city = history
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=imperial&appid=" 
+        + myApi
+    )
+    .then((response) => {
+        return response.json();
+      })
+      .then((data) => displayCurrent(data));
+      forcast()
+    }
+}
 //function update search history
 
-//function to get history from local storage
-
+// get history from local storage
+localStorage.setItem('city', JSON.stringify(city))
 
 //function for geolocation and use function to make calls above
 
-// function to create buttons on search history
+function geolocation {
+
+}
 
