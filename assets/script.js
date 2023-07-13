9ce0190149698ecdfa1d4c2ff42618177e803c93
Jacob <Jakelausier7878@gmail.com>
2023-07-13 17:48:27 -0400

    fixed loop and buttons

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,20 @@ var myApi = "0798b2213871a3519588b870cc87e7a2"
 //url with api key
 var weatherUrl = ""
 //add event listner
-$(buttonEl).on('click',getWeather)
+$(buttonEl).on('click',function(event){
+    event.preventDefault();
+    var city = (document.getElementById('city').value.trim())
+    getWeather(city)
+})
 
 // function to display weather from fetch
 //--need icon, temp,humidity,date,windspeed
 //create elements for each
 
 
+let storeHistory = JSON.parse(localStorage.getItem('cities')) || [];
 
-function getWeather(event){
-    event.preventDefault();
-    var city = (document.getElementById('city').value.trim())
-    localStorage.setItem('city', JSON.stringify(city))
+function getWeather(city){
  
     fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" 
@@ -32,7 +34,9 @@ function getWeather(event){
         return response.json();
       })
       .then((data) => displayCurrent(data));
-      forcast()
+      forcast(city);
+
+      
     
 }
 
@@ -45,75 +49,76 @@ function getWeather(event){
     document.getElementById('temp').innerText = temp +" °F"
     document.getElementById('description').innerText = description
     document.getElementById('humidity').innerText = "humidity: " + humidity;
-   forcast()
-   displayHistory()
+// if(!searchHisotry.includes(name)){
+//     localStorage.setItem('citties', JSON.stringify(searchHisotry))
+//     localStorage.getItem(searchHisotry)
+
+// }
+   saveHistory(name)
+//    displayHistory()
 
 }
-    
-
-
-
-
-
 // function to display 5 day forcast
- function forcast(){
-    
-    
-     fetch(
-        
-        
-    
-        "https://api.openweathermap.org/data/2.5/forecast?q=" 
+ function forcast(city){
+     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" 
          + city 
          + "&units=imperial&appid=" 
           + myApi
      ).then((response) => {
         return response.json();
-        
       })
-      .then((data) => function displayFuture(){
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity}= data.main;
-        const {date}= data.list.dt
+      .then((data) => {
         
-     let pastCity= []
-    
-        for( var i = 0; i <data.length; i++){
-            var pastSearch = city;
-            pastCity.push({text: pastSearch})
-            historyDiv= document.createElement('div')
-            historyDate = document.createElement('h2')
-            historyTemp =  document.createElement('h2')
-            historyImg =  document.createElement('img')
-            historyDes =  document.createElement('h2')
-            historyHum =  document.createElement('h2')
-    
-            
-           historyDiv.id = "future"
-           historyDate.id ="date"
-           historyTemp.id = "temp-2"
-           historyImg.id = "icon-2"
-           historyDes.id = "description-2"
-           historyHum.id = "humidity-2"
-    
-            
-           fiveDay.innerText = name
-               document.getElementById('date').innerText = date
-               document.getElementById('icon-2').src = "https://openweathermap.org/img/wn/" + icon + ".png"
-               document.getElementById('temp-2').innerText = temp +" °F"
-               document.getElementById('description-2').innerText = description
-               document.getElementById('humidity-2').innerText = "humidity: " + humidity;
-            
-            pastSearch.classList.add('btn, col-4, shadow-0 , p-2, text-dark-emphasis, fs-4, bg-info-subtle')
-            historyDiv.innerText = pastSearch
-            fiveDay.append(historyDiv, historyDate, historyTemp, historyImg, historyDes, historyHum)
-           
-        }
+        futureDays(data);
     });
 }
         
+function futureDays(data){
+    console.log(data);
+    let pastCity= []
     
+    for( var i = 7; i < data.list.length; i += 8){
+        const {name}= data.city
+        const {icon, description} = data.list[i].weather[0];
+        const {temp, humidity}= data.list[i].main
+        const {date} = data.list[i].dt_txt
+        
+        console.log(temp);
+        console.log(humidity);
+        console.log(icon);
+        console.log(description);
+
+        // var pastSearch = searchHisotry;
+        // pastCity.push({text: pastSearch})
+       var historyDiv= document.createElement('div')
+       var historyDate = document.createElement('h2')
+       var historyTemp =  document.createElement('h2')
+       var historyImg =  document.createElement('img')
+       var historyDes =  document.createElement('h2')
+       var historyHum =  document.createElement('h2')
+
+        
+       historyDiv.id = "future"
+       historyDate.id ="date"
+       historyTemp.id = "temp-2"
+       historyImg.id = "icon-2"
+       historyDes.id = "description-2"
+       historyHum.id = "humidity-2"
+
+        
+       fiveDay.innerText = name
+       historyDate.innerText = date
+       historyImg.src = "https://openweathermap.org/img/wn/" + icon + ".png"
+       historyTemp.innerText = temp +" °F"
+       historyDes.innerText = description
+       historyHum.innerText = "humidity: " + humidity;
+        
+    //     pastSearch.classList.add('btn, col-4, shadow-0 , p-2, text-dark-emphasis, fs-4, bg-info-subtle')
+    //     historyDiv.innerText = pastSearch
+        fiveDay.append(historyDiv, historyDate, historyTemp, historyImg, historyDes, historyHum)
+       
+    }
+}
 //        historyDiv =   document.createElement('div')
 //        historyDate = document.createElement('h2')
 //        historyTemp =  document.createElement('h2')
@@ -141,39 +146,52 @@ function getWeather(event){
 // }
 
 //array of search history
-let storeHistory = [];
 
-//function to display search history
-// function to create buttons on search history
+
+
+function saveHistory(city){
+    if(!storeHistory.includes(city)){
+storeHistory.push(city)
+    localStorage.setItem('cities', JSON.stringify(storeHistory))
+    displayHistory()
+}
+}
+
+// //function to display search history
+// // function to create buttons on search history
  function displayHistory(){
     
-    for( var i = 0; i <localStorage.length; i++){
-        var history = JSON.parse(localStorage.getItem(localStorage.key(i))) 
-        storeHistory.push(history)
+    for( var i = 0; i <storeHistory.length; i++){
+        var history = storeHistory[i] 
+        // storeHistory.push(history)
         historyList = document.createElement('div')
-        // history.classList.add('btn, col-4, shadow-0 , p-2, text-dark-emphasis, fs-4, bg-info-subtle')
+        historyList.setAttribute('class' ,'btn  shadow-0  p-2 text-dark-emphasis fs-4 bg-info-subtle m-4')
         historyList.innerText = history
         listEl.append(historyList)
-        $(listEl).on('click', weatherAgain)
+        $(listEl).on('click', function(event){
+            var historyCity = event.target.innerText
+            getWeather(historyCity)
+        })
         
     }
-    function weatherAgain(event){
-        event.preventDefault();
-
-        var city = history
-    fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=imperial&appid=" 
-        + myApi
-    )
-    .then((response) => {
-        return response.json();
-      })
-      .then((data) => displayCurrent(data));
-      forcast()
-    }
 }
+//     function weatherAgain(event){
+//         event.preventDefault();
+
+//         var city = history
+//     fetch(
+//         "https://api.openweathermap.org/data/2.5/weather?q=" 
+//         + city 
+//         + "&units=imperial&appid=" 
+//         + myApi
+//     )
+//     .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => displayCurrent(data));
+//       forcast(city)
+//     }
+// }
 //function update search history
 
 // get history from local storage
